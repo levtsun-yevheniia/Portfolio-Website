318b8321e95181f5a4c576002784c860ac8eff70
levtsun-yevheniia <levtsun.yevheniia@gmail.com>
2024-03-24 23:17:00 +0300

    Projects, Source code screens

---
+++ src/mainComp/SourceCode.js
@@ -0,0 +1,21 @@
+function SourceCode() {
+  return (
+    <div className="sourcecode">
+      <div className="container">
+        <div className="title">
+          <div className="heading">
+            <p>Source code</p>
+          </div>
+        </div>
+        <div className="head">
+          <p>Visit my GitHub repository for a comprehensive view of my coding work.</p>
+        </div>
+        <div className="button_block">
+          <div className="btn btn--second">Show me</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SourceCode;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,6 +2,8 @@ import Header from './../mainComp/Header';
 import First from './../mainComp/First';
 import AboutMe from '../mainComp/AboutMe';
 import Skills from '../mainComp/Skills';
+import Projects from '../mainComp/Projects';
+import SourceCode from '../mainComp/SourceCode';
 
 function Main() {
   return (
@@ -10,6 +12,8 @@ function Main() {
       <First />
       <AboutMe />
       <Skills />
+      <Projects />
+      <SourceCode />
     </div>
   );
 }
